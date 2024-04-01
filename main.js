971d1894c067ec1df7376ecb7b65e6e97a7d58e5
Aayushi Mishra <mishraaayushi66@gmail.com>
2024-04-01 07:58:45 +0530

    changes

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-
 // nav background
 let header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
     header.classList.toggle("shadow", window.scrollY > 0)
 })
-
 //Filter
 $(document).ready(function () {
     $(".filter-item").click(function () {
